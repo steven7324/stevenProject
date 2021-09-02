79019e004a1476f510c0ab1086e8b0a79e6629e2
Steven Arbelaez Palacio <steven.arbelaezp@perficient.com>
2021-09-02 08:46:37 -0500

    Add project with junit 5 and selenium

---
+++ src/test/java/com/google/test/GoogleSeleniumTest.java
@@ -0,0 +1,37 @@
+package com.google.test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSeleniumTest {
+
+    @BeforeAll
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Test
+    public void searchSomethingInGooglePage() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        WebElement txtSearchField = driver.findElement(By.name("q"));
+//        WebElement btnSearch = driver.findElement(By.name("btnK"));
+
+        txtSearchField.sendKeys("cars");
+        txtSearchField.sendKeys(Keys.ENTER);
+
+        WebElement verifyElement = driver.findElement(By.id("pTwnEc"));
+        Assertions.assertTrue(verifyElement.isDisplayed());
+
+        driver.quit();
+    }
+
+}
