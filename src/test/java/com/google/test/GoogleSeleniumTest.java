77e1dce909ecde8fec7b84753217f346f9f70dfa
Steven Arbelaez Palacio <steven.arbelaezp@perficient.com>
2021-09-09 13:27:12 -0500

    Add System properties to run the project with chrome or firefox

--- src/test/java/com/google/test/GoogleSeleniumTest.java
+++ src/test/java/com/google/test/GoogleSeleniumTest.java
@@ -9,32 +9,47 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
+import java.sql.Time;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 
 public class GoogleSeleniumTest {
 
+    protected static final String BROWSER = System.getProperty("BROWSER", "firefox");
+    public WebDriver driver;
+
     @BeforeAll
     public static void setupClass() {
         WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
     }
 
     @Test
-    public void searchSomethingInGooglePage() {
-        WebDriver driver = new ChromeDriver();
+    public void searchSomethingInGooglePageWithChrome() throws InterruptedException {
+        switch (BROWSER) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+        }
+
         driver.manage().window().maximize();
         driver.get("https://www.google.com");
         WebElement txtSearchField = driver.findElement(By.name("q"));
-//        WebElement btnSearch = driver.findElement(By.name("btnK"));
-
         txtSearchField.sendKeys("cars");
         txtSearchField.sendKeys(Keys.ENTER);
-
+        TimeUnit.SECONDS.sleep(5);
         WebElement verifyElement = driver.findElement(By.id("pTwnEc"));
-        Assertions.assertTrue(verifyElement.isDisplayed());
 
+        Assertions.assertTrue(verifyElement.isDisplayed());
         driver.quit();
     }
 
