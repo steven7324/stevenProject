61c93efe7e2ac3d0935fbce1610ae621c72d78ed
Steven Arbelaez Palacio <steven.arbelaezp@perficient.com>
2021-09-10 17:19:03 -0500

    Add runner to the workflow to include firefox and I separated the browser options

---
+++ src/test/java/com/google/test/BrowserUtil.java
@@ -0,0 +1,28 @@
+package com.google.test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserUtil {
+
+    WebDriver driver = null;
+
+    public String BROWSER = System.getProperty("BROWSER", "firefox");
+
+
+    public WebDriver browser() {
+        switch (BROWSER) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+        }
+        return driver;
+    }
+}
--- src/test/java/com/google/test/GoogleSeleniumTest.java
+++ src/test/java/com/google/test/GoogleSeleniumTest.java
@@ -8,39 +8,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 
-import java.sql.Time;
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 
 public class GoogleSeleniumTest {
 
-    protected static final String BROWSER = System.getProperty("BROWSER", "firefox");
-    public WebDriver driver;
-
-    @BeforeAll
-    public static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-        WebDriverManager.firefoxdriver().setup();
-    }
+    public WebDriver driver = new BrowserUtil().browser();
 
     @Test
     public void searchSomethingInGooglePageWithChrome() throws InterruptedException {
-        switch (BROWSER) {
-            case "chrome":
-                driver = new ChromeDriver();
-                break;
-            case "firefox":
-                driver = new FirefoxDriver();
-                break;
-        }
-
         driver.manage().window().maximize();
         driver.get("https://www.google.com");
         WebElement txtSearchField = driver.findElement(By.name("q"));
