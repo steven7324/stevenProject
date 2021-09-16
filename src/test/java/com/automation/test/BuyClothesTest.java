d9efd6a7024e7a96145430fa6209b8af7669af38
Steven Arbelaez Palacio <steven.arbelaezp@perficient.com>
2021-09-16 12:05:29 -0500

    Modify project to avoid using scroll

--- src/test/java/com/automation/test/BuyClothesTest.java
+++ src/test/java/com/automation/test/BuyClothesTest.java
@@ -4,45 +4,46 @@ import com.automation.test.util.BrowserManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.MoveTargetOutOfBoundsException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
+import java.awt.*;
 import java.time.Duration;
 import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
 
 
-public class BuyPracticeTest {
+public class BuyClothesTest {
 
     public WebDriver driver = BrowserManager.build();
-    Wait<WebDriver> wait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(30)).ignoring(NoSuchElementException.class);
+    Wait<WebDriver> wait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(30))
+            .pollingEvery(Duration.ofSeconds(3))
+            .ignoring(NoSuchElementException.class)
+            .ignoring(MoveTargetOutOfBoundsException.class);
+
     Actions actions = new Actions(driver);
 
     @Test
-    public void addSomethingToCartAndBuyIt() throws InterruptedException {
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.get("http://automationpractice.com/");
+    public void addSomethingToCartAndBuyIt() {
+        driver.navigate().to("http://automationpractice.com/");
         //addToCart and Checkout
-        WebElement btnViewCloth = driver.findElement(By.xpath("//ul[@id='homefeatured']/li[1]/div"));
+        WebElement btnViewCloth = driver.findElement(By.xpath("(//ul[@id='homefeatured']/li//a[@class='product-name'])[1]"));
 
-        //Scroll to Element
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true);", btnViewCloth);
+        btnViewCloth.sendKeys(Keys.DOWN);
+
         wait.until(ExpectedConditions.visibilityOf(btnViewCloth));
         actions.moveToElement(btnViewCloth).perform();
 
-        WebElement btnAddToCart = driver.findElement(By.xpath("(//span[text()='Add to cart'])[1]"));
-        actions.moveToElement(btnAddToCart).perform();
+        WebElement btnAddToCart = driver.findElement(By.xpath("(//span[normalize-space()='Add to cart'])[1]"));
         wait.until(ExpectedConditions.visibilityOf(btnAddToCart));
         btnAddToCart.click();
 
         WebElement btnCheckout = driver.findElement(By.xpath("//a[@title='Proceed to checkout']"));
-        js.executeScript("arguments[0].scrollIntoView(true);", btnCheckout);
         wait.until(ExpectedConditions.visibilityOf(btnCheckout));
         btnCheckout.click();
 
@@ -50,6 +51,7 @@ public class BuyPracticeTest {
         btnCheckoutSummary.click();
         //login
         WebElement txtEmail = driver.findElement(By.id("email"));
+        wait.until(ExpectedConditions.visibilityOf(txtEmail));
         txtEmail.sendKeys("steven7324@yopmail.com");
         WebElement txtPassword = driver.findElement(By.id("passwd"));
         txtPassword.sendKeys("Steven21");
@@ -75,7 +77,6 @@ public class BuyPracticeTest {
 
         //Verify the order is completed
         WebElement verifyElement = driver.findElement(By.xpath("//p[@class='cheque-indent']"));
-        js.executeScript("arguments[0].scrollIntoView(true);", verifyElement);
         wait.until(ExpectedConditions.visibilityOf(verifyElement));
         Assertions.assertEquals(verifyElement.getText(), "Your order on My Store is complete.");
         driver.quit();
